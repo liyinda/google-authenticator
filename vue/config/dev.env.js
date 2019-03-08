c5f1357670071d06849a1a328141b93090d6e4d5
lfp <bjlfp@163.com>
2019-03-08 16:33:26 +0800

    host change

--- vue/config/dev.env.js
+++ vue/config/dev.env.js
@@ -1,6 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://101.200.42.56:8080"'
-  // BASE_API: '"http://52.196.214.171:8888/passport"'
+  // BASE_API: '"http://192.168.1.2:8888/passport"'
 }
